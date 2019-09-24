e39bb116fc99aedf84027f3a3ea4378072fc2078
gzonelee <>
2019-09-24 14:35:32 +0900

    .

--- PictureInput.vue
+++ PictureInput.vue
@@ -256,10 +256,8 @@ export default {
     },
     onFileChange (e, prefill) {
       let files = e.target.files || e.dataTransfer.files
+        console.log(files)
       if (!files.length) {
-        return
-      }
-      if (files.length === 1 && files[0].size === 0 && files[0].name === "fokCamera.png") {
         return
       }
       if (files[0].size <= 0 || files[0].size > this.size * 1024 * 1024) {
